ea92efacfda6ebf0f66770af34339404ef2e91ce
Darwin Cabarrubias <cabarrubiasdarwin98@gmail.com>
2023-05-09 09:41:26 +0800

    Initial commit

---
+++ src/composables/skills.js
@@ -0,0 +1,68 @@
+import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+axios.defaults.baseURL = "http://task-manager.test/api/v1";
+
+export default function useSkills() {
+  const skills = ref([]);
+  const skill = ref({});
+  const errors = ref({});
+  const router = useRouter();
+
+  const getSkills = async () => {
+    const response = await axios.get("skills");
+    skills.value = response.data.data;
+  };
+
+  const getSkill = async (id) => {
+    const response = await axios.get(`skills/${id}`);
+    skill.value = response.data.data;
+  };
+
+  const storeSkill = async (data) => {
+    errors.value = {}; // Reset errors object
+
+    try {
+      await axios.post("skills", data);
+      await router.push({ name: "index" });
+    } catch (error) {
+        
+        if (error.response.status === 422) {
+            errors.value = error.response.data.message;
+        }
+    }
+  };
+
+  const updateSkill = async (id) => {
+    try {
+      await axios.put(`skills/${id}`, skill.value);
+      await router.push({ name: "index" });
+    } catch (error) {
+      if (error.response.status === 422) {
+        errors.value = error.response.data.message;
+      }
+    }
+  };
+
+  const destroySkill = async (id) => {
+    if (!window.confirm("Are you sure you want to destroy?")) {
+      return;
+    }
+
+    await axios.delete(`skills/${id}`);
+    await getSkills();
+  };
+
+  // Return the skills, skill, and errors objects along with the functions
+  return {
+    skills,
+    skill,
+    errors, // Include the errors object in the return value
+    getSkills,
+    getSkill,
+    storeSkill,
+    updateSkill,
+    destroySkill,
+  };
+}
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/skills',
+      name: 'index',
+      component: () => import('../views/skills/index.vue')
+    },
+    {
+      path: '/skills/create',
+      name: 'create',
+      component: () => import('../views/skills/create.vue')
+    },
+    {
+      path: '/skills/:id/edit',
+      name: 'edit',
+      component: () => import('../views/skills/edit.vue')
+    }
+    
+  ]
+})
+
+export default router
